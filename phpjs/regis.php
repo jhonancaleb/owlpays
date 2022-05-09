f79688c1f4f619394bc4a3fc842ba3d608e9a491
jhonan <jhonancalebm@gmail.com>
2022-05-09 08:36:03 -0500

    algunos estilos y el title de registrarse

--- phpjs/regis.php
+++ phpjs/regis.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../img/buho.png">
     <link rel="stylesheet" href="../css/style2.css">
-    <title>Owlpays|Inicio Sesion</title>
+    <title>Owlpays|Registrarse</title>
 </head>
 <body>
     <section>
