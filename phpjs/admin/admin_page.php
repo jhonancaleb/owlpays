812ee68c78f0323fd5438d556bfb4336118dde21
jhonan <jhonancalebm@gmail.com>
2022-05-13 16:02:19 -0500

    creando pagina admin

---
+++ phpjs/admin/admin_page.php
@@ -0,0 +1,79 @@
+<?php
+    session_start();
+    $user=$_SESSION['username'];
+    if($user==null || $user==''){
+        header("Location:../index.php");
+        die();
+    }
+    include "conexion.php";
+    $db=new database();
+    $query=$db->connect()->prepare('SELECT dni_cliente AS username,nombres AS name FROM usuarios WHERE dni_cliente=:username');
+    $query->execute([':username' => $user]);
+    $row=$query->fetch(PDO::FETCH_ASSOC);
+    $idd=$row['username'];
+    $nombres=$row['name'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="../../img/buho.png" type="image/png">
+    <script src="https://kit.fontawesome.com/a8527aea5d.js" crossorigin="anonymous"></script><!-- //mi kit en font awesome -->
+    <link rel="stylesheet" href="../../css/menu.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Owlpays Admin</title>
+</head>
+<body>
+    <div class="body">
+        <div class="menu-barra" id="menu-barra">
+            <div class="con-bar">
+                <div class="bar" id="btn" title="Menú"><i class="fa-solid fa-bars" id="icon_bar"></i></div>
+            </div>
+            
+            <a href="admin_page.php" class="logo" id="logo">
+                <img src="../../img/buho.png" alt="">
+                <label id="ie">Owlpays Admin</label>
+            </a>
+            <div class="opciones">
+                <a class="opcion" href="" title="Usuarios" target="cuerpo"><span><i class="fa-solid fa-user"></i></span>            <h2 class="letra">Usuarios</h2></a>
+                <a class="opcion" href="" title="Suscripciones" target="cuerpo"><span><i class="fa-solid fa-chalkboard-user"></i></span> <h2 class="letra">Suscripciones</h2></a>
+                <a class="opcion" href="proveedores.php" title="Proveedores" target="cuerpo"><span><i class="fa-solid fa-clipboard"></i></span>       <h2 class="letra">Proveedores</h2></a>
+                <!-- <a class="opcion" href="../grados/grados.php" title="Grados" target="cuerpo"><span><i class="fa-solid fa-graduation-cap"></i></span>  <h2 class="letra"></h2></a>
+                <a class="opcion" href="../asistencia/asistencia.php" title="Asistencia" target="cuerpo"><span><i class="fa-solid fa-calendar-check"></i></span>  <h2 class="letra">Asistencia</h2></a> -->
+            </div>
+            <div class="usuario">
+                <div class="datos">
+                    <img class="usuario-desa" src="../../img/usuario.png" alt="">
+                    <label class="usuario-desa"><?php echo $nombres?></label>
+                </div>
+                <a class="salir" href="../cerrar_sesion.php" title="Cerrar Sesión"><span><i class="fa-solid fa-right-from-bracket"></i></span></a>
+            </div>
+        </div>
+        <iframe name="cuerpo" src="" frameborder="0"></iframe>
+    </div>
+   <script>
+       /* window,alert("hola") */
+        const boton=document.querySelector("#btn");
+        let menu=document.querySelector("#menu-barra");
+        let logo=document.querySelector("#logo");
+        let iconbar=document.querySelector("#icon_bar");
+        let letra_insti=document.querySelector("#ie");
+        let opcion=document.querySelectorAll(".letra");
+        let usuario=document.querySelectorAll(".datos")[0];
+        let salir=document.querySelectorAll(".salir")[0];
+        function barra(){
+            /* window,alert("hola"); */
+            boton.classList.toggle("desactive");
+            iconbar.classList.toggle("desactive");
+            menu.classList.toggle("oculto");
+            letra_insti.classList.toggle("desactive");
+            usuario.classList.toggle("oculto");
+            salir.classList.toggle("solo");
+            opcion.forEach((item)=> 
+            item.classList.toggle("oculto")); 
+        }
+        boton.onclick = barra; //en proceso
+   </script> 
+</body>
+</html>
---
+++ phpjs/admin/proveedores.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="proveedor.css">
+    <script src="https://kit.fontawesome.com/a8527aea5d.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content-formu">
+            <form class="formu" action="proveedores.php" method="post" enctype="multipart/form-data">
+                <h1>Agrega proveedores</h1>
+                <input type="text" name="tx_nombre" id="" class="input" placeholder="Nombre del proveedor" required>
+                <select name="tx_categoria" id="" class="input" placeholder="Categoria" required>
+                    <option value="STREAMING">STREAMING</option>
+                    <option value="EDUCACION">EDUCACION</option>
+                    <option value="DISEÑO">DISEÑO</option>
+                    <option value="NUBE">NUBE</option>
+                </select>
+                <input type="text" name="tx_plan1" id="" class="input" placeholder="Plan1" required>
+                <input type="text" name="tx_plan2" id="" class="input" placeholder="Plan2" required>
+                <input type="text" name="tx_plan3" id="" class="input" placeholder="Plan3" required>
+                <div class="file-img">
+                    <input type="file" name="img" id="file">
+                    <label for="file"><i class="fa-solid fa-upload"></i> Subir imagen</label>
+                </div>
+                <?php
+                    include 'conectar.php';
+                    if(!empty($_POST['tx_nombre'])=="")
+                    {  }
+                    else
+                    {
+                        $nombre=$_POST["tx_nombre"];
+                        $categoria=$_POST["tx_categoria"];
+                        $plan1=$_POST["tx_plan1"];
+                        $plan2=$_POST["tx_plan2"];
+                        $plan3=$_POST["tx_plan3"];
+                        /* imagen */
+                        $tamanoimg=$_FILES["img"]["size"];
+                        $imagen=fopen($_FILES["img"]["tmp_name"], "r");
+                        $imagen_bin=fread($imagen,$tamanoimg);
+                        $imagen_bin=mysqli_escape_string($db_connect,$imagen_bin);
+                        $sql_prod ="INSERT INTO proveedores(nombre,categoria,plan1,plan2,plan3,image) values('$nombre','$categoria','$plan1','$plan2','$plan3','$imagen_bin')";  
+                        $result_prod = $db_connect -> query($sql_prod);
+                    }       
+                ?>
+                <input type="submit" class="submit" value="Agregar">
+            </form>
+            <div class="img">
+                <img src="../../img/undraw1.svg" alt="">
+            </div>
+        </div>
+        <div class="content-proves">
+            <div class="buscador">
+                <ul>
+                    <li><a href="proveedores.php">TODOS</a></li>
+                    <li><a href="prove_cat.php?cat=STREAMING" target="frame_cat" onclick="mostrar();">STREAMING</a></li>
+                    <li><a href="prove_cat.php?cat=DISEÑO" target="frame_cat" onclick="mostrar();">DISEÑO</a></li>
+                    <li><a href="prove_cat.php?cat=EDUCACIÓN" target="frame_cat" onclick="mostrar();">EDUCACION</a></li>
+                    <li><a href="prove_cat.php?cat=NUBE" target="frame_cat" onclick="mostrar();">NUBE</a></li>
+                </ul>
+                <form action="proveedores.php" method="post">
+                    <input type="text" name="tx_prove" id="" class="input" placeholder="Escriba el proveedor" required>
+                    <input type="submit" value="Buscar" class="submit">
+                </form>
+            </div>
+            <table id="tabla">
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Categoría</th>
+                <th>Plan1</th>
+                <th>Plan2</th>
+                <th>Plan3</th>
+                <th>Logo</th>
+                <?php
+                    include 'conectar.php';
+                    if(!empty($_POST['tx_prove'])=="")
+                    {  
+                        $sql_prod ="SELECT * FROM proveedores";  
+                        $result_prod = $db_connect -> query($sql_prod);
+                        if($result_prod -> num_rows > 0) {
+                            while ( $rows = $result_prod -> fetch_assoc() ) {
+                                $code = $rows['id_proveedor'];
+                                $name = $rows['nombre'];
+                                $cate = $rows['categoria'];
+                                $plan1 = $rows['plan1'];
+                                $plan2 = $rows['plan2'];
+                                $plan3 = $rows['plan3'];
+                                $imagen = $rows['image'];
+                                echo'
+                                 <tr>
+                                     <td>'.$code.'</td>
+                                     <td class="nombres">'.$name.'</td>
+                                     <td>'.$cate.'</td>
+                                     <td>'.$plan1.'</td>
+                                     <td>'.$plan2.'</td>
+                                     <td>'.$plan3.'</td>
+                                     <td><img src="data:image/jpg;base64,'.base64_encode($imagen).'" alt="'.$name.'"></td>
+                                 </tr>
+                                ';
+                           }
+                        } 
+                    }
+                    else
+                    {
+                        $prove=$_POST['tx_prove'];
+                        $sql_prod ="SELECT * FROM proveedores WHERE nombre LIKE '%".$prove."%'";  
+                        $result_prod = $db_connect -> query($sql_prod);
+                        if($result_prod -> num_rows > 0) {
+                            while ( $rows = $result_prod -> fetch_assoc() ) {
+                                $code = $rows['id_proveedor'];
+                                $name = $rows['nombre'];
+                                $cate = $rows['categoria'];
+                                $plan1 = $rows['plan1'];
+                                $plan2 = $rows['plan2'];
+                                $plan3 = $rows['plan3'];
+                                $imagen = $rows['image'];
+                                echo'
+                                 <tr>
+                                     <td>'.$code.'</td>
+                                     <td class="nombres">'.$name.'</td>
+                                     <td>'.$cate.'</td>
+                                     <td>'.$plan1.'</td>
+                                     <td>'.$plan2.'</td>
+                                     <td>'.$plan3.'</td>
+                                     <td><img src="data:image/jpg;base64,'.base64_encode($imagen).'" alt="'.$name.'"></td>
+                                 </tr>
+                                ';
+                           }
+                        } 
+                        else{
+                            echo'
+                                <tr>
+                                    <td colspan=7>
+                                        <div class="nohay">
+                                            <img src="https://cdn-icons.flaticon.com/png/128/5445/premium/5445197.png?token=exp=1652474705~hmac=daf06c97731f4c8b8196ec40b8e823d6" alt="vacio">
+                                            <p>No hay proveedores que coincidan con "'.$prove.'"</p>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ';
+                        }
+                    }   
+                ?>
+            </table>
+            <iframe src="" frameborder="0" name="frame_cat" id="Iframe"></iframe>
+        </div>
+    </div>
+    <script>
+        // Selecting the iframe element
+        let frame = document.getElementById("Iframe");
+          
+        /* // Adjusting the iframe height onload event
+        frame.onload = function()
+        // function execute while load the iframe
+        {
+          // set the height of the iframe as 
+          // the height of the iframe content
+          frame.style.height = 
+          frame.contentWindow.document.body.scrollHeight + 'px';
+           
+  
+         // set the width of the iframe as the 
+         // width of the iframe content
+         frame.style.width  = 
+         frame.contentWindow.document.body.scrollWidth+'px';
+              
+        }  */
+        let tabla = document.getElementById("tabla");
+        function mostrar(){
+            tabla.style.display = "none";
+            frame.style.display = "block";
+        }
+        </script>
+</body>
+</html>
