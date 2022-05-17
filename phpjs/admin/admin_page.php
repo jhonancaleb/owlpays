adc4554147bfbbd613bf18ef341f33ed88b35f9e
jhonan <jhonancalebm@gmail.com>
2022-05-17 13:09:16 -0500

    sesion start redireccion al inicio de sesion si no esta logeado

--- phpjs/admin/admin_page.php
+++ phpjs/admin/admin_page.php
@@ -2,7 +2,7 @@
     session_start();
     $user=$_SESSION['username'];
     if($user==null || $user==''){
-        header("Location:../index.php");
+        header("Location:../inicio_sesion.php");
         die();
     }
     include "conexion.php";
