8992580de937489b63f0502a5e4a6c7ae3455d1e
jhonan <jhonancalebm@gmail.com>
2022-06-10 19:48:38 -0500

    DELETE CODIGO INECESARIO

--- phpjs/admin/proveedores.php
+++ phpjs/admin/proveedores.php
@@ -154,23 +154,6 @@
     <script>
         // Selecting the iframe element
         let frame = document.getElementById("Iframe");
-          
-        /* // Adjusting the iframe height onload event
-        frame.onload = function()
-        // function execute while load the iframe
-        {
-          // set the height of the iframe as 
-          // the height of the iframe content
-          frame.style.height = 
-          frame.contentWindow.document.body.scrollHeight + 'px';
-           
-  
-         // set the width of the iframe as the 
-         // width of the iframe content
-         frame.style.width  = 
-         frame.contentWindow.document.body.scrollWidth+'px';
-              
-        }  */
         let tabla = document.getElementById("tabla");
         function mostrar(){
             tabla.style.display = "none";
